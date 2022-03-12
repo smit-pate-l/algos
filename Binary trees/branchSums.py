a22d08a24af64c5683f7b2de0dd197cc7efb8560
Smit Patel <smitp520.sp@gmail.com>
2022-03-12 13:06:28 -0600

    new question added

---
+++ Binary trees/branchSums.py
@@ -0,0 +1,24 @@
+  def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def branchSums(root):
+	sums = []
+	calculateBranchSums(root,0,sums)
+	return sums
+
+def calculateBranchSums(node, runningSum, sums):
+	if node is None:
+		return
+	
+	newRunningSum = runningSum + node.value
+	print(newRunningSum)
+	if node.left is None and node.right is None:
+		sums.append(newRunningSum)
+		return
+	
+	calculateBranchSums(node.left, newRunningSum, sums)
+	calculateBranchSums(node.right, newRunningSum, sums)
+
