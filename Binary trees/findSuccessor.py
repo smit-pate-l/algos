b0da6c44d990a7986bd896cea9fe133607381aab
Smit Patel <smitp520.sp@gmail.com>
2022-03-15 09:38:49 -0500

    new question added

--- Binary trees/findSuccessor.py
+++ Binary trees/findSuccessor.py
@@ -26,3 +26,29 @@ def  inOrderTraversal(node, order = []):
 	
 	return order
 	
+# Solution 2
+class BinaryTree:
+    def __init__(self, value, left=None, right=None, parent=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+
+def findSuccessor(tree, node):
+    if node.right is not None:
+		return findLeftMostChild(node.right)
+	return findRightMostParent(node)
+
+def findLeftMostChild(node):
+	currentNode = node
+	while currentNode.left is not None:
+		currentNode = currentNode.left
+		
+	return currentNode
+
+def findRightMostParent(node):
+	currentNode = node
+	while currentNode.parent is not None and currentNode.parent.right == currentNode:
+		currentNode = currentNode.parent
+	return currentNode.parent
