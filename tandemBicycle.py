b529021b5944c861ac26db7e1360c05133be8613
Smit Patel <smitp520.sp@gmail.com>
2022-03-23 11:04:57 -0500

    new question added

--- tandemBicycle.py
+++ tandemBicycle.py
@@ -11,3 +11,26 @@ def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
 		total += max(blueShirtSpeeds[idx], redShirtSpeeds[idx])
 
     return total
+
+
+def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
+	total = 0
+    redShirtSpeeds.sort()
+	blueShirtSpeeds.sort()
+	if fastest == True:
+		reverseArrayInPlace(redShirtSpeeds)
+		
+
+	for idx in range(len(blueShirtSpeeds)):
+		total += max(blueShirtSpeeds[idx], redShirtSpeeds[idx])
+
+    return total
+
+def reverseArrayInPlace(array):
+	start = 0
+	end = len(array) - 1
+	
+	while start < end:
+		array[start], array[end] = array[end], array[start]
+		start += 1
+		end -= 1
